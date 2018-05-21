3adbad3bf48931423d3d23035ee5f8f7a13d1d5d
Watife <watife14@yahoo.com>
2018-05-21 23:38:33 +0100

    Completed hero head and intro block

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,10 @@
-const gulp = require ('gulp'),
-watch = require ('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-variables = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import')
-
-gulp.task('default', () => {
-    console.log('Hurray - you created a Gulp task');
-})
-
-gulp.task('html', () => {
-    console.log('Imagine something useful with html');
-});
-
-gulp.task('styles', () => {
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, variables, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-})
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('watch', () => {
-    watch('./app/index.html', () => {
-        gulp.start('html');
-    });
-    watch('./app/assets/styles/**/*.css', () => {
-        gulp.start('styles')
-    })
-})
+
+
+
+
 
 
